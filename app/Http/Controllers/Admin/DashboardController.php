bff91f961a3dd7e41e613fe5cd4ae32300fab415
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-29 11:14:19 +0200

    feat: Update index method in DashboardController to check user authentication

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    // Auth
     // Index
     public function index()
     {
-        return "Index del mio blog";
-    }
 
+
+        $user = Auth::user();
+        return Auth::check();
+        // return "Index del mio blog";
+
+
+    }
     // Profile
     public function profile()
     {
