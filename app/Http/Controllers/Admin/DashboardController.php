6eff68a91dce5de6f589c02cbf15cc1c59a0e8f7
PaoloVisone <polvis.pv.22@gmail.com>
2025-06-04 19:36:45 +0200

    Add index and profile methods to DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        return Auth::check();
+    }
+
+    public function profile()
+    {
+        return "Pagina profilo";
+    }
 }
