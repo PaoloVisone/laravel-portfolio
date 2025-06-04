771aa9785d6acd75e52411b5422e00f0ac274bdd
PaoloVisone <polvis.pv.22@gmail.com>
2025-06-04 22:09:07 +0200

    Add Bootstrap CSS and JS links to projects layout

--- resources/views/layouts/projects.blade.php
+++ resources/views/layouts/projects.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title")</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
@@ -14,5 +15,6 @@
 
         @yield("content")
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
