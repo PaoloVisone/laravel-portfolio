166670520f0a4a17f5a2e77c56c6827e22f86a30
PaoloVisone <polvis.pv.22@gmail.com>
2025-06-04 22:09:12 +0200

    Refactor projects index view to display project details in a structured layout

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,23 @@
 @section("title", "Tutti i progetti")
 
 @section("content")
-
-hello!
-
+<div class="container py-5">
+    <h2 class="text-center m-4">I miei progetti</h2>
+        
+    @foreach($projects as $project)
+    <div class="card text-center m-4">
+        <div class="card-header">{{ $project->client }} - {{ $project->category }}</div>
+  
+        <div class="card-body">
+            <h5 class="card-title">{{ $project->title }}</h5>
+            <p class="card-text">{{ $project->content }}</p>
+        </div>
+ 
+        <div class="card-footer text-body-secondary">
+            <a href="{{ route('projects.show', $project->id) }}" class="btn btn-primary">Dettagli</a>
+        </div>
+    </div>
+        @endforeach
+    </div>
+</div>
 @endsection
